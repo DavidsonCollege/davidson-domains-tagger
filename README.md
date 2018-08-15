f481883e824a23bd2e083268ebe69c87517c285b
john-michael-murphy <j.johnmichaelmurphy@gmail.com>
2018-08-14 20:34:01 -0400

    add readme and pull tags.json from github instead of explictly defining it in plugin.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Davidson Domains Community Portal
+
+## About
+This is a Wordpress plugin that exposes metadata about sites to [Site Maker](https://github.com/woodwardtw/sites), a Wordpress plugin that grabs data about Davidson Domains sites and displays them on the [Davidson Domains Community Portal](http://domains.davidson.edu/community).
--- davidson-domains-meta.php
+++ davidson-domains-meta.php
@@ -10,7 +10,7 @@ Version: 1.0
 
 */
 
-define('DDM_LIST', 'https://raw.githubusercontent.com/DavidsonCollege/davidson-domains-meta/master/tags');
+define('DDM_LIST', 'https://raw.githubusercontent.com/DavidsonCollege/davidson-domains-meta/documentation-and-upgrades/tags.json');
 //create settings page
 add_action('admin_menu', 'ddm_options_page');
 
@@ -25,7 +25,7 @@ function ddm_options_page_html()
 
   <div class="wrap">
     <h1><?= esc_html(get_admin_page_title()); ?></h1>
-    
+
     <?php
     if (get_bloginfo('version') < 4.8){
       ?><p style='color: red'>You are running Wordpress version <?= get_bloginfo('version') ?>. This plugin works only with Wordpress 4.8 or above. </p><?php
@@ -35,66 +35,59 @@ function ddm_options_page_html()
         function saveSettings(){alert('Your Settings have been Saved!')}
     </script>
     <form action="<?= plugins_url('updatesettings.php', __FILE__ ); ?>" method="post" onsubmit="return saveSettings()">
-  
+
   <?php
   // Add nonce to form.
   wp_nonce_field('edit_tags', 'ddm_tag_nonce');
-  
+
   //Retrieve currently selected settings from WP database
   $ddm_tags = get_option('ddm_tags');
-  
+
   //Retrieve JSON from CDN (GitHub in our case)
-  //$response = wp_remote_get( DDM_LIST );
-  
-  //json array
-  $response =  '{
-                "Who are you?": ["Faculty","Staff","Student"],
-                "What type of site is this? (You can choose more than one.)": ["English","CIS","Digital Studies","Digital Storytelling","Portfolio","Experiment","Course Site","Oral Histories","Archives","Podcast","JEC Mellon Funded","Research/Scholarship","Organization"],
-                "If you are a student what is your expected year of graduation?": ["2018","2019","2020","2021"]
-                }';
-  
+  $response = wp_remote_get( DDM_LIST );
+
   //Convert JSON object to PHP object
   $response = json_decode($response);
   // See http://php.net/manual/en/function.json-decode.php
-  
-  //Iterate through object. Each key becomes a section. Iterate through the associated array. 
+
+  //Iterate through object. Each key becomes a section. Iterate through the associated array.
   foreach ($response as $key => $attributes) {
-    
+
     //Section Title
     ?><h3><?=$key?></h3><?php
-    
+
     //Section attributes
     for ($x = 0; $x < sizeof($attributes); $x++) {
-      
+
       //Checkbox label
       ?><label for="<?=$attributes[$x]?>"> <?=$attributes[$x]?></label><?php
-      
+
       //Check the box if user has previously checked
       if ( in_array($attributes[$x], $ddm_tags) ){
         ?>
         <input id="<?=$attributes[$x]?>" name='tags[]' value ='<?=$attributes[$x]?>' type="checkbox" checked>
         <?php
       }
-      
+
       //Don't check the box if user hasn't previously checked
       else {
         ?>
         <input id="<?=$attributes[$x]?>" name='tags[]' value ='<?=$attributes[$x]?>' type="checkbox">
         <?php
       }
-      
+
     }
-    
+
   }
-  
-  
-  
+
+
+
   //Save on subject
   settings_fields('wporg_options');
   do_settings_sections('wporg');
   submit_button('Save Settings');
   ?>
-  
+
 </form>
   </div>
   <?php
