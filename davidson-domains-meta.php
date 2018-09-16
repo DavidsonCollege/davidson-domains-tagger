6204e9030d7d49fa70a6414e449a6a7e12d5304b
john-michael-murphy <j.johnmichaelmurphy@gmail.com>
2018-09-16 18:13:29 -0400

    wrap plugin in init function, remove unecessary files, esc and validate text, add link to community portal, add namespace in front of functions.

--- davidson-domains-meta.php
+++ davidson-domains-meta.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Davidson Domains Meta
+Plugin Name: Davidson Domains Tagger
 Plugin URI: https://github.com/DavidsonCollege/davidson-domains-meta
 Description: Opt into Davidson Domains
 Author: John-Michael Murphy
@@ -10,112 +10,106 @@ Version: 1.2
 
 */
 
-/* SETTINGS PAGE */
-function ddm_options_page_html()
-{
-  if (!current_user_can('manage_options')) {
-    status_header(403);
-    ?><h1>Forbidden</h1><?php
-    return;
+
+add_action('init','davidson_domains_do');
+function davidson_domains_do(){
+  // register settings page
+  add_action('admin_menu', 'davidson_domains_options_page');
+  function davidson_domains_options_page()
+  {
+    add_submenu_page(
+      'tools.php',
+      'Davidson Domains Tagger',
+      'Davidson Domains',
+      'manage_options',
+      'davidson-domains-tagger',
+      'davidson_domains_settings_page'
+    );
   }
-  ?>
-  <div class="wrap">
-    <h1><?= esc_html(get_admin_page_title()); ?></h1>
-    <?php
-    if (get_bloginfo('version') < 4.8){
-      ?><p style='color: red;'>You are running Wordpress version <?= get_bloginfo('version') ?>. This plugin works only with Wordpress 4.8 or above. </p><?php
+
+  // Register form fields in database.
+  if(current_user_can('manage_options')) {
+    add_action( 'admin_init', 'davidson_domain_register_field' );
+  }
+
+  function davidson_domain_register_field() {
+    register_setting( 'davidson-domains-tagger', 'davidson-domains-tags', 'davidson_domains_sanitize');
+  }
+
+  function davidson_domains_settings_page()
+  {
+    if (!current_user_can('manage_options')) {
+      status_header(403);
+      ?><h1>Forbidden</h1><?php
+      return;
     }
-    ?>
-     <script>
-        function onSubmit(){ alert('Your Settings have been Saved!'); }
-    </script>
-    <form action="<?= plugins_url('updatesettings.php', __FILE__ ); ?>" method="post" onsubmit="return onSubmit()">
 
-  <?php
-  // Add nonce to form.
-  wp_nonce_field('edit_tags', 'ddm_tag_nonce');
-
-  //Retrieve currently selected settings from WP database
-  $ddm_tags = get_option('ddm_tags');
-  if ( empty($ddm_tags) ) {
+    $string = file_get_contents(dirname(__FILE__) . '/tags.json');
+    $all_tags = json_decode($string, true);
+    //Retrieve currently selected settings from WP database
+    $selected_tags = get_option('davidson-domains-tags');
     // If the array doesn't exist, then create an empty one, so in_array doesn't bawk.
-    $ddm_tags = [];
+    if ( empty($selected_tags) ) { $selected_tags = []; }
+    else { $selected_tags = davidson_domains_sanitize($selected_tags); }
+
+    ?>
+    <div class="wrap">
+      <h1><?= esc_html(get_admin_page_title()); ?></h1>
+      <p>Tag your site with this plugin to be featured on the <a target="_blank" href="https://domains.davidson.edu/community/">Davidson Domains Community Portal</a>.</p>
+      <?php
+      if (get_bloginfo('version') < 4.8){
+        ?><p style='color: red;'>You are running Wordpress version <?= get_bloginfo('version') ?>. This plugin works only with Wordpress 4.8 or above. </p><?php
+      }
+      ?>
+      <form action="options.php" method="post">
+        <?php if( isset($_GET['settings-updated']) ) { ?>
+          <div class="notice notice-success is-dismissible">
+            <p><?php _e('Saved! Your site is tagged and ready to for prime time!'); ?></p>
+          </div>
+          <?php
+        }
+        settings_fields('davidson-domains-tagger');
+        do_settings_sections('davidson-domains-tagger');
+        //Iterate through object. Each key becomes a section. Iterate through the associated array.
+        foreach ($all_tags as $key => $attributes) {
+          //Section Title
+          ?><h3><?=$key?></h3><?php
+          //Section attributes
+          for ($x = 0; $x < sizeof($attributes); $x++) {
+            //Check the box if user has previously checked
+            if ( in_array($attributes[$x], $selected_tags) ){
+              ?>
+              <input id="<?=esc_attr($attributes[$x])?>" name='davidson-domains-tags[]' value ='<?=esc_attr($attributes[$x])?>' type="checkbox" checked>
+              <?php
+            }
+            //Don't check the box if user hasn't previously checked
+            else {
+              ?>
+              <input id="<?=esc_attr($attributes[$x])?>" name='davidson-domains-tags[]' value ='<?=esc_attr($attributes[$x])?>' type="checkbox">
+              <?php
+            }
+            //Checkbox label
+            ?><label for="<?=$attributes[$x]?>"> <?=$attributes[$x]?></label><?php
+          }
+        }
+        submit_button('Save Tags');
+        ?>
+      </form>
+    </div>
+    <?php
   }
 
-  $string = file_get_contents(dirname(__FILE__) . '/tags.json');
-  $tags = json_decode($string, true);
-
-  //Iterate through object. Each key becomes a section. Iterate through the associated array.
-  foreach ($tags as $key => $attributes) {
-
-    //Section Title
-    ?><h3><?=$key?></h3><?php
-
-    //Section attributes
-    for ($x = 0; $x < sizeof($attributes); $x++) {
-
-      //Check the box if user has previously checked
-      if ( in_array($attributes[$x], $ddm_tags) ){
-        ?>
-        <input id="<?=$attributes[$x]?>" name='tags[]' value ='<?=$attributes[$x]?>' type="checkbox" checked>
-        <?php
-      }
-
-      //Don't check the box if user hasn't previously checked
-      else {
-        ?>
-        <input id="<?=$attributes[$x]?>" name='tags[]' value ='<?=$attributes[$x]?>' type="checkbox">
-        <?php
-      }
-
-      //Checkbox label
-      ?><label for="<?=$attributes[$x]?>"> <?=$attributes[$x]?></label><?php
-
-    }
-
+  //Append ddm_tags to /wp-json
+  add_filter('rest_index', 'davidson_domains_add_tags_to_endpoint');
+  function davidson_domains_add_tags_to_endpoint($response){
+    $data = $response->data;
+    $data['ddm_tag'] = get_option('davidson-domains-tags');
+    $response->set_data($data);
+    return $response;
   }
 
-  //Save on subject
-  settings_fields('wporg_options');
-  do_settings_sections('wporg');
-  submit_button('Save Settings');
-  ?>
-
-</form>
-  </div>
-  <?php
+  function davidson_domains_sanitize($input){
+    return array_map('sanitize_text_field', wp_unslash($input));
+  }
 }
-
-// register settings page
-add_action('admin_menu', 'davidson_domains_options_page');
-function davidson_domains_options_page()
-{
-  add_submenu_page(
-    'tools.php',
-    'Davidson Domains Meta',
-    'Davidson Domains Meta',
-    'manage_options',
-    'davidson-domains-meta',
-    'ddm_options_page_html'
-  );
-}
-
-//Append ddm_tags to /wp-json
-add_filter('rest_index', 'davidson_domains_add_meta_to_endpoint');
-function davidson_domains_add_meta_to_endpoint($response){
-  $data = $response->data;
-  $data['ddm_tag'] = get_option('ddm_tags');
-  $response->set_data($data);
-  return $response;
-}
-
-// if (!wp_verify_nonce($_POST['ddm_tag_nonce'], 'edit_tags')) {
-//     status_header(400, "Invalid nonce");
-//     return;
-// }
-//
-// $tags = $_POST['tags'];
-// update_option('ddm_tags', $tags, yes);
-// echo header('Location: ' . $_SERVER['HTTP_REFERER']);
-
 ?>
